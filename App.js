000104d2fee9b79e67ab2dfed907bcbebc16c513
Kavitha-Nagaraju <nagarajukavibhavi@gmail.com>
2024-08-12 22:35:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+    <h1>This isw first programme in React.Js</h1>
+    <div id="div1">
+      <img src="https://th.bing.com/th?id=OIP.U_VJuupQohwnzXcKMztqWgHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" width="30%" height="300px"/>
+      <p>The company raised INR 1.5 crore (equivalent to under $250,000) in funding from LetsVenture and Ah! Ventures in 2014.[11] In 2017, Testbook secured series A funding from Matrix Partners. Prior to this, Testbook had also received funding from S. Chand and Utsav Somani of AngelList, among other investors.[12] In 2020, Testbook raised another INR 60 Cr ($8.3 Mn) from Iron Pillar.[9][13][14]</p>
+    </div>
+    <h2>Figure.1</h2>
+    <div id="div2">
+      <p>The company raised INR 1.5 crore (equivalent to under $250,000) in funding from LetsVenture and Ah! Ventures in 2014.[11] In 2017, Testbook secured series A funding from Matrix Partners. Prior to this, Testbook had also received funding from S. Chand and Utsav Somani of AngelList, among other investors.[12] In 2020, Testbook raised another INR 60 Cr ($8.3 Mn) from Iron Pillar.[9][13][14]</p>
+      <img src="https://th.bing.com/th?id=OIP.PikUdeXdPP5f3FAwKZYtlQHaJQ&w=223&h=279&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" width="80%" height="300px"/>
+    </div>
+    <h3>Figure.2</h3>
+    <div id="div3">
+      <img src="https://th.bing.com/th?id=OIP.qDvAlhidTBzXiGyDfq_O0gHaE7&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"/>
+      <h4>Image1</h4>
+      <p>Subjects. Peer reviewed, openly licensed, and totally free online! OpenStax textbooks come with free, aligned student and instructor resources. Explore our library of resources and …</p>
+    </div>
+    <div id="div4">
+      <img src="https://th.bing.com/th?id=OIP.FEqv7YYMNjXtrVYqo7HHzAHaE7&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"/>
+      <h4>Figure2</h4>
+      <p>Subjects. Peer reviewed, openly licensed, and totally free online! OpenStax textbooks come with free, aligned student and instructor resources. Explore our library of resources and …</p>
+    </div>
+    <div id="div5">
+      <img src ="https://th.bing.com/th?id=OIP.xDVmMCG193U2wO2swWkyGQHaJQ&w=223&h=279&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" width="90%" height="270px"/>
+      <h4>Figure3</h4>
+      <p>Subjects. Peer reviewed, openly licensed, and totally free online! OpenStax textbooks come with free, aligned student and instructor resources. Explore our library of resources and …</p>
+    </div>
+    <div id="div6">
+      <div className="div6"> 1</div>
+      <div className="div6">2</div>
+      <div className="div6">3</div>
+      <div className="div6">4</div>
+      <div className="div6">5</div>
+    </div>
+    <div id="div7">
+      <div className="div7"> 1</div>
+      <div className="div7">2</div>
+      <div className="div7">3</div>
+      <div className="div7">4</div>
+      <div className="div7">5</div>
+      <div className="div7">6</div>
+      <div className="div7">7</div>
+      <div className="div7">8</div>
+      <div className="div7">9</div>
+      <div className="div7">10</div>
+      <div className="div7">10</div>
+      <div className="div7">10</div>
+    </div>
+    <div id="div8">
+      <div className="div8"> 1</div>
+      <div className="div8">2</div>
+      <div className="div8">3</div>
+      <div className="div8">4</div>
+      <div className="div8">5</div>
+      <div className="div8">6</div>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
